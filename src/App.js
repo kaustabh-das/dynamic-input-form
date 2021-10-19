09d603e376b31604b4c52c6e62da57d81815e9c4
kaustabh-das <kaustabhkumar1997@gmail.com>
2021-10-19 00:15:22 -0700

    dynamic input form first commit

---
+++ src/App.js
@@ -0,0 +1,116 @@
+import React, { useState } from "react";
+import "./styles/App.css";
+import { Container,
+         Paper, 
+         Box, 
+         Button,
+         Grid,
+         TextField,
+         IconButton
+         } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+// import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import { Trash as TrashIcon } from "react-feather"
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100vw",
+    height: "100vh",
+    backgroundColor: theme.palette.grey[300],
+    paddingTop: theme.spacing(5),
+  },
+  inputGroup: {
+    marginBottom: theme.spacing(1)
+  }
+}));
+
+function App() {
+
+  const classes = useStyles();
+
+  const userTemplate = {name: "", email: "", phone: "", address: ""};
+  const [users, setUsers] = useState([userTemplate]);
+
+  const addUser = () => {
+    setUsers([...users, userTemplate]);
+  }
+
+  const onChange = (e, index) => {
+    const updatedUsers = users.map((user, i) => index === i ? 
+      Object.assign(user, {[e.target.name]: e.target.value}) : user)
+
+    setUsers(updatedUsers)
+  }
+
+  const removeUser = (index) => {
+    const filteredUsers = [...users];
+    filteredUsers.splice(index, 1);
+    setUsers(filteredUsers);
+    // console.log(index)
+  }
+
+  return (
+    <Container className={classes.root}>
+      <Paper component={Box} p={4}>
+        {
+          users.map((user, index)=> (
+            <Grid container spacing={3} key={index} className={classes.inputGroup}> 
+              <Grid item md={3}> 
+                <TextField
+                label="Name"
+                placeholder="Enter Your Name"
+                variant="outlined"
+                name="name"
+                onChange={e => onChange(e, index)}
+                value={user.name}
+                fullWidth 
+                />
+              </Grid>
+              <Grid item md={3}> 
+                <TextField
+                label="E-mail"
+                placeholder="Enter Your E-mail"
+                variant="outlined"
+                name="email"
+                onChange={e => onChange(e, index)}
+                value={user.email}
+                fullWidth 
+                />
+              </Grid>
+              <Grid item md={2}> 
+                <TextField
+                label="Phone"
+                placeholder="Enter Your Phone No"
+                variant="outlined"
+                name="phone"
+                onChange={e => onChange(e, index)}
+                value={user.phone}
+                fullWidth 
+                />
+              </Grid>
+              <Grid item md={3}> 
+                <TextField
+                label="Address"
+                placeholder="Enter Your Address"
+                variant="outlined"
+                name="address"
+                onChange={e => onChange(e, index)}
+                value={user.address}
+                fullWidth 
+                />
+              </Grid>
+              <Grid item md={1}> 
+                <IconButton color="secondary" onClick={() => removeUser(index)}>  
+                  <TrashIcon />
+                </IconButton>
+              </Grid>
+            </Grid>
+          ))
+        }
+        <Button variant="contained" color="primary" onClick={addUser}>Add User</Button>
+      </Paper>
+    </Container>
+  );
+}
+
+export default App;
